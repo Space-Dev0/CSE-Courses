cb242b9dcd00428adfd4abea74016a17fb937239
Space-Dev0 <179112172+Space-Dev0@users.noreply.github.com>
2025-02-25 09:49:14 +0530

    fix: edit .gitignore

--- CS103-OOP/Labs/Lab0/CS24B1017_Lab0_q3.cpp
+++ CS103-OOP/Labs/Lab0/CS24B1017_Lab0_q3.cpp
@@ -1,99 +1,55 @@
 #include <iostream>
 #include <string>
+
 class Calculator
 {
 private:
-    std::string expression;
+    double solver(std::string_view);
+    double evaluate(double, double, char);
+    std::string_view subexpression(std::string_view, int *);
 
 public:
     Calculator();
-    double solver(std::string);
+    Calculator(std::string_view expression);
     ~Calculator();
 };
 
 Calculator::Calculator()
 {
+    std::string expression;
     std::cin >> expression;
     std::cout << "Result: " << solver(expression) << "\n";
 }
-// Trying to solve using recursion
 
-double Calculator::solver(std::string expr)
+Calculator::Calculator(std::string_view expression)
 {
-    double evaluates, num1{0}, num2{0};
-    char ch{'+'};
-    bool isnum1 = true;
-    for (int i = 0; i < expr.length(); i++)
-    {
-        if (48 <= expr.at(i) && expr.at(i) <= 58 && isnum1)
-        {
-            num1 *= 10;
-            num1 += (static_cast<int>(expr.at(i)) - 48);
-            continue;
-        }
-        else if (48 <= expr.at(i) && expr.at(i) <= 58 && !isnum1)
-        {
-            num2 *= 10;
-            num2 += (static_cast<int>(expr.at(i)) - 48);
-            continue;
-        }
-        else if (expr.at(i) == '+' || expr.at(i) == '-' ||
-                 expr.at(i) == '*' || expr.at(i) == '/')
-        {
-            ch = expr.at(i);
-            isnum1 = false;
-            continue;
-        }
+    std::cout << "Result: " << solver(expression) << "\n";
+}
 
-        if (expr.at(i) == '(')
-        {
-            int j{i}, count{1};
-            while (count != 0)
-            {
-                j++;
-                if (expr.at(j) == '(')
-                    count++;
-                else if(expr.at(j) == ')')
-                    count--;
-            }
-            std::string subexpr = expr.substr(i + 1, j - i - 1);
-            if (isnum1)
-            {
-                num1 = solver(subexpr);
-                i = j;
-                isnum1 = !isnum1;
-                continue;
-            }
-            num2 = solver(subexpr);
-            i = expr.length();
-        }
-        // if (1 > num1 || num1 > 1000)
-        // {
-        //     std::cout << "Error: Invalid inputs!\n";
-        //     return;
-        // }
-        if (ch != '+' && ch != '-' && ch != '*' && ch != '/')
-        {
-            std::cout << "Error: Invalid operator!\n";
-            break;
-        }
-        // if (1 > num1 || num1 > 1000)
-        // {
-        //     std::cout << "Error: Invalid inputs!\n";
-        //     return;
-        // }
+std::string_view Calculator::subexpression(std::string_view expr, int *i)
+{
+    int j{*i}, count{1};
+    while (count != 0)
+    {
+        (*i)++;
+        if (expr.at(*i) == '(')
+            count++;
+        else if (expr.at(*i) == ')')
+            count--;
     }
+    return expr.substr(j + 1, *i - j - 1);
+}
+
+double Calculator::evaluate(double num1, double num2, char ch)
+{
     switch (ch)
     {
     case '+':
         return num1 + num2;
-        break;
     case '-':
         return num1 - num2;
-        break;
     case '*':
         return num1 * num2;
-        break;
     case '/':
         if (num2 == 0)
         {
@@ -101,11 +57,54 @@ double Calculator::solver(std::string expr)
             break;
         }
         return num1 / num2;
-        break;
     default:
+        std::cout << "Error: Invalid operator!\n";
         break;
     }
     return 0;
+}
+
+// Trying to solve using recursion
+
+double Calculator::solver(std::string_view expr)
+{
+    double evaluates, num1{0}, num2{0};
+    char ch{'+'};
+    bool isnum1 = true;
+    for (int i = 0; i < expr.length(); i++)
+    {
+        if (48 <= expr.at(i) && expr.at(i) <= 58)
+        {
+            int num = isnum1 ? num1 : num2;
+            num *= 10;
+            num += (static_cast<int>(expr.at(i)) - 48);
+            isnum1 ? num1 = num : num2 = num;
+            continue;
+        }
+        else if (expr.at(i) == '+' || expr.at(i) == '-' || expr.at(i) == '*' || expr.at(i) == '/')
+        {
+            if (!isnum1)
+            {
+                num1 = evaluate(num1, num2, ch);
+                num2 = 0;
+            }
+            ch = expr.at(i);
+            isnum1 = false;
+            continue;
+        }
+        if (expr.at(i) == '(')
+        {
+            std::string_view subexpr{subexpression(expr, &i)};
+            if (isnum1)
+            {
+                num1 = solver(subexpr);
+                isnum1 = !isnum1;
+                continue;
+            }
+            num2 = solver(subexpr);
+        }
+    }
+    return evaluate(num1, num2, ch);
 }
 
 Calculator::~Calculator()
